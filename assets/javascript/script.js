d76b212118171bd1f49a44555af7b019dd55f424
Ckopei <cameron@kopels.com>
2019-09-15 17:33:39 -0400

    gif loads on button click. gifs pause and unpause upon click.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -16,8 +16,9 @@ function appendButtons() {
 appendButtons()
 
 // 3. When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
-function displayGifs() {
+$(document).on("click", "button", function () {
     var show = $(this).text().trim();
+    console.log(show)
     // ask how to target rating
     // var rating = $(document)
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=wBCi3R0zS2IBZBegGU2ND9oIM6BWFETs&q=" + show + "&limit=10&offset=0&rating=PG-13&lang=en"
@@ -28,34 +29,40 @@ function displayGifs() {
         method: "GET"
     }).then(function (response) {
         $(".gifsHere").empty();
-        for (let i = 0; i <= 10; i++) {
-            console.log(response);
-            
+        console.log(response);
+        for (let i = 0; i <= 9; i++) {
             //create variables for all things being pulled.
-            let gifUrlStatic = response.data[i].images.fixed_width_still;
-            let gifUrlAnimated = response.data[i].images.fixed_width;
+            let gifUrlStatic = response.data[i].images.fixed_width_still.url;
+            let gifUrlAnimated = response.data[i].images.fixed_width.url;
             let rating = response.data[i].rating;
             //create div to hold each gif and it's rating
             let gifDiv = $("<div class='gifsDiv'>")
-            var img = $("<img class='gifs'>")
+            var img = $("<img class='gifs img-responsive'>")
             img.attr("src", gifUrlStatic);
             img.attr("data-state", "still");
             img.attr("data-still", gifUrlStatic);
             img.attr("data-animate", gifUrlAnimated);
             gifDiv.append(img);
             $(".gifsHere").append(gifDiv);
-
-
         };
     })
-
-}
-
-$("button").on("click", function () {
-    displayGifs()
 })
 
 // 4. When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+$(document).on("click", "img", function(){
+    //retrieving the img's state to use below
+    var state = $(this).attr("data-state")
+    //if else for data state and src swapping
+    //using true/false based off "still" or "animate"
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr ("data-state", "animate");
+    }
+    else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still")
+    }
+})
 
 // 5. Under every gif, display its rating (PG, G, so on).
 //    * This data is provided by the GIPHY API.
