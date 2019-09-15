6e25f001371e5ccadcf40c69bccb95bf1b7b7b35
Ckopei <cameron@kopels.com>
2019-09-15 16:27:35 -0400

    initial HTML layout. completed JS function for rendering buttons.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,10 +4,34 @@
 
 
 //1. Before you can make any part of your site work, you need to create an array of strings, each one related to a topic that interests you. Save it to a variable called `topics`.
+var shows = ["The Office", "Parks and Rec", "Loony Toons", "The Good Place", "Friends", "Its Always Sunny", "House MD", "Game of Thrones", "Seinfeld", "Workaholics"]
 // * We chose animals for our theme, but you can make a list to your own liking.
+
+// $("button").on("click", function () {
+//     $.ajax({
+//         url: "https://api.giphy.com/v1/gifs/search?api_key=wBCi3R0zS2IBZBegGU2ND9oIM6BWFETs&q=the office&limit=10&offset=0&rating=G&lang=en",
+//         method: "GET"
+//     }).then(function (response) {
+//         console.log(response)
+//     })
+// })
+
 
 // 2. Your app should take the topics in this array and create buttons in your HTML.
 //    * Try using a loop that appends a button for each string in the array.
+function appendButtons() {
+    //clearing that div so it wont appends twice. the goal of this function is to clear and reappend every one in the array every single time it's called.
+    $("#buttonsHere").empty();
+    //loop through array, append buttons for array items.
+    for (let i = 0; i<shows.length; i++) {
+        var buttons = $("<button");
+        //setting the buttons classes and type to bootstrap.
+        buttons.attr("type", "button");
+        buttons.addClass("btn btn-dark");
+        buttons.text(shows[i])
+        $("#buttonsHere").append(buttons)
+    }
+}
 
 // 3. When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
 
